3b3cd4d325d9d0a0ba5d68746ce9c43569ef34fd
liuyuchen012 <123623085+liuyuchen012@users.noreply.github.com>
2025-01-18 16:55:31 +0800

    Update worker-vless.js

--- worker-vless.js
+++ worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'e9664bea-f633-4b5c-a0ba-cbf448c86a77';
 
-let proxyIP = 'cdn.xn--b6gac.eu.org'; // workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org
+let proxyIP = 'proxyip.us.fxxk.dedyn.io '; // workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org
 
 if (!isValidUUID(userID)) {
 	throw new Error('uuid is not valid');
